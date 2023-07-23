efa72df5f77a66cb5dc3105741e0957741729fff
Dan_Pro <danpr080704@gmail.com>
2023-07-23 22:58:34 +0300

    Updating CodeDeploy start_script.sh

--- codedeploy-scripts/start_script.sh
+++ codedeploy-scripts/start_script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mv /tmp/module4-thin.war /usr/local/tomcat/webapps/ROOT.war
+echo -e "export JAVA_OPTS=\"-Dspring.profiles.active=prod\"" > /usr/local/tomcat/bin/setenv.sh
 /usr/local/tomcat/bin/catalina.sh start
